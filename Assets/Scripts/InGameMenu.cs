dcd42a6b6f66cab4b02b1d30520618c1cddd0d64
Wing Chau <ccw.wingc@gmail.com>
2017-01-13 10:00:08 +0000

    implement pause menu

--- Assets/Scripts/InGameMenu.cs
+++ Assets/Scripts/InGameMenu.cs
@@ -3,12 +3,14 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class GameOver : MonoBehaviour {
+public class InGameMenu : MonoBehaviour {
 
     [SerializeField]
     private Text m_time;
     [SerializeField]
     private GameObject m_gameOverMenu;
+    [SerializeField]
+    private GameObject m_pauseMenu;
     [SerializeField] Text m_stopwatch;
 
 	// Use this for initialization
@@ -18,6 +20,26 @@ public class GameOver : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.JoystickButton7)) {
+            if (!m_pauseMenu.activeInHierarchy && !m_gameOverMenu.activeInHierarchy) {
+                m_pauseMenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.JoystickButton0)) {
+            if (m_pauseMenu.activeInHierarchy) {
+                m_pauseMenu.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.JoystickButton1)) {
+            if (m_pauseMenu.activeInHierarchy) {
+                SceneManager.LoadScene(0);
+            }
+        }
+
         if (CheckpointController.currentLap == 2) {
             m_gameOverMenu.SetActive(true);
             m_time.text = m_stopwatch.text;
