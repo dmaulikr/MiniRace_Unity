a5cb0a0802f41d1bdc6be7195e64c37c52f6bb83
Wing Chau <ccw.wingc@gmail.com>
2017-01-13 12:53:50 +0000

    implement camera rotation

---
+++ Assets/Scripts/TurnCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnCamera : MonoBehaviour {
+
+    private Transform m_transform;
+    private float m_angle = 45.0f;
+    private Vector3 m_turnAngle;
+
+	// Use this for initialization
+	void Start () {
+        m_transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_turnAngle = m_transform.localEulerAngles;
+        m_turnAngle.y = m_angle * Input.GetAxis("Camera");
+
+        m_transform.transform.localEulerAngles = m_turnAngle;
+	}
+}
