1d967f3b755d8f1391a5cb2b2eade8f8ed4c925a
Wing Chau <ccw.wingc@gmail.com>
2017-01-13 08:48:47 +0000

    implement checkpoint scripts

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+    static Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+         playerTransform = GameObject.FindWithTag("Player").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            if (transform == CheckpointController.checkpointA[CheckpointController.currentCheckpoint].transform) {
+                Debug.Log("get in");
+                if (CheckpointController.currentCheckpoint + 1 < CheckpointController.checkpointA.Length) {
+                    if (CheckpointController.currentCheckpoint == 0) {
+                        CheckpointController.currentLap++;
+                    }
+                    CheckpointController.currentCheckpoint++;
+                } else {
+                    CheckpointController.currentCheckpoint = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointController : MonoBehaviour {
+
+    public Transform[] checkpointArray;
+    public static Transform[] checkpointA;
+    public static int currentCheckpoint = 0;
+    public static int currentLap = 0;
+    public int Lap;
+
+    private Vector3 startPos;
+
+    void Start() {
+        startPos = GetComponent<Transform>().position;
+        currentCheckpoint = 0;
+        currentLap = 0;
+    }
+
+    void Update() {
+        Lap = currentLap;
+        checkpointA = checkpointArray;
+    }
+}
